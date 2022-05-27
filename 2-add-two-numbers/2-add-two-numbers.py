0912950e59c17bf8af4c61a284ba3d675ed85617
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-05-27 13:02:19 -0700

    Time: 83 ms (61.42%), Space: 14 MB (44.22%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,26 +5,28 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        #get the two numbers as a string and reverse and sum
-        num1, num2 = '',''
-        while l1:
-            num1 += str(l1.val)
-            l1 = l1.next
-        while l2:
-            num2 += str(l2.val)
-            l2 = l2.next
+        #logicaly
+        total = 0
+        carry = 0
+        dum = res = ListNode()
         
-        total = int(num1[::-1]) + int(num2[::-1])
+        #while theres still values in linked list or if theres a carry remaining
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            total = v1 + v2 + carry
+            #you dont need to check if carry > 9 bc if less than 10 will eval to 0
+            # // 10 will get the first digit in any int
+            carry = total // 10
+            #total only needs the ones place
+            total = total % 10
+            #add the value to the resulting LinkedList
+            res.next = ListNode(total)
+            res = res.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
         
-        total = str(total)
-        total = total[::-1]
         
-        dum = ListNode(0)
-        res = dum
-        
-        for ch in total:
-            dum.next = ListNode(int(ch))
-            dum = dum.next
-        
-        return res.next
+        return dum.next
+                
         
