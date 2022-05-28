70ac1e3c8411bc5c7aa1ceebf8de51925bd69ae3
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-05-27 19:13:23 -0700

    Time: 176 ms (58.12%), Space: 15.2 MB (34.52%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -4,12 +4,30 @@ class Solution:
         key point: distant number in range [0,n]? Cyclic Sort!
         loop through the array and plave each number at its correct index if there isnt a match that is the solution?
         ex:
-        [3,0,1]
+        [2,0,1]
         length = 3
         make sure everynumber between 0,3 is in there if not return that num
         '''
         
-        length = len(nums)
-        for num in range(length + 1):
-            if num not in nums:
-                return num
+        length, i = len(nums), 0
+        
+        #cylic sort
+        while i < length:
+            j = nums[i]
+            if j < length and nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+            
+        
+        #linear scan of missing num
+        for i, num in enumerate(nums):
+            if num != i:
+                return i
+        
+        return length
+                
+                
+            
+        
+        
