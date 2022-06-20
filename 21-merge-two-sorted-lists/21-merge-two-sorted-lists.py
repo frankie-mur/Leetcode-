eb87210f9714d10ee47de4b19adc8c2a5ac9abf3
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-06-20 09:16:13 -0700

    Time: 100 ms (5.41%), Space: 14 MB (31.90%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,23 +5,34 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        #create a dummy node to have a pointer to the head
+        '''
+        U-Given two linked lists merge them in sorted order
+        M- two pointer
+        P- have a pointer at each head and create a new list adding the smallest values from both, increment as taken
+        I- python
+        R- passes test casses, thinking about a way to use constant space...hmmm
+        E- Space: O(n) -> for the result linked list
+           Time: O(n) -> for passes through both l1 and l2
+        
+        '''
         dum = ListNode(0)
         res = dum
-    
-        
         while l1 and l2:
-            #append to res using two pointer in non-decreasing order
-            if l1.val < l2.val:
-                res.next, l1 = l1, l1.next
+            if l1.val > l2.val:
+                res.next = ListNode(l2.val)
+                l2 = l2.next
             else:
-                res.next, l2 = l2, l2.next
-                
+                res.next = ListNode(l1.val)
+                l1 = l1.next
+            
             res = res.next
-        #if there are left over values
-        res.next = l1 or l2
+        
+        if l1:
+            res.next = l1
+        if l2:
+            res.next = l2
         
         return dum.next
             
-            
-            
+        
+        
