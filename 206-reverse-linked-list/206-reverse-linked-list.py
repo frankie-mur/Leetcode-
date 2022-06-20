56158666c36c484bdf61ac516024d66c8ced0666
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-06-20 09:21:18 -0700

    Time: 69 ms (17.30%), Space: 15.6 MB (28.05%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,6 +5,17 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        U- given a linked list return its inverse
+        M- iterative 
+        P- Keep track of the prev node in the LL and set the next node to its prev, do this while there is a next
+        I-python
+        R- this solution always works, I need to learn how to do recursively..is there an advantage?
+        E- Time: O(n) for iterating through the LL
+           Space: O(1) no extra space used
+        
+        '''
+        
         prev = None
         while head:
             temp = head.next
@@ -13,9 +24,3 @@ class Solution:
             head = temp
         
         return prev
-            
-            
-            
-            
-            
-            
