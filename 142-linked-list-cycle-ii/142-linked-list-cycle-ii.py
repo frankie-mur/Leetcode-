e9854d2146a372663593f67bacc4223de21f0c7e
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-06-22 13:55:12 -0700

    Time: 48 ms (96.76%), Space: 17.3 MB (94.40%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        U-If a list contains a cycle return where that cycle starts
+        M-Fast and slow pointer
+        P-First find if there is a cycle, if there is then start a new pointer from the beginning and increment till the previous pointer and the new one collide, this is the start. Can be proven with math 
+        I- Python
+        R-
+        E-
+        '''
+        slow,fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                #CYCLE!
+                start = head
+                while start != slow:
+                    start,slow = start.next,slow.next
+                
+                return start
+        
+        return 
+        
+        
