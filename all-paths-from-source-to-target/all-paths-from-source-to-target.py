ace98052260d2af5da977af24713c097cb5f0267
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-07-07 15:12:49 -0700

    Time: 126 ms, Memory: 15.8 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,21 +1,12 @@
 class Solution:
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        start,end = 0, len(graph) - 1
-        res = []
-        def dfs(vertex, cur_path,res):
-            cur_path.append(vertex)
-            
-            if vertex == end:
-                res.append(cur_path.copy())
-            #dont need to check visited bc directed graph
-            for neig in graph[vertex]:
-                dfs(neig, cur_path, res)
-                #pop here to remove backtrack cur_path
-                cur_path.pop()
-        
-        dfs(start,[],res)
-        
-        return res
+    def allPathsSourceTarget(self, G: List[List[int]]) -> List[List[int]]:
+        ans, q = [], deque([[0]])
+        while q:
+            path = q.popleft()
+            if path[-1] == len(G)-1: ans.append(path)
+            else:
+                q.extend(path + [child] for child in G[path[-1]])
+        return ans
                 
             
         
