ca5d82b3f164e09eecec1045b1ddc88387a87fc6
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-07-10 11:17:54 -0700

    Time: 115 ms (19.44%), Space: 13.9 MB (97.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,11 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        min_cost = [0] * (len(cost) + 1)
+        a = 0
+        b = cost[0]
         
-        for i in range(2,len(cost) + 1):
-            one_step = min_cost[i-1] + cost[i-1]
-            two_step = min_cost[i-2] + cost[i-2]
-            
-            min_cost[i] = min(one_step, two_step)
-            
-        return min_cost[-1]
+        for i in range(1, len(cost)):
+            a, b = b, min(a + cost[i], b + cost[i])
+        
+        return min(a, b)
+        
