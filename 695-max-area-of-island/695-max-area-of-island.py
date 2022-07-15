169b498549660f844f12f197c47da4beb3ef7c2b
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-07-14 20:24:03 -0700

    Time: 176 ms (74.04%), Space: 17.7 MB (29.69%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -4,29 +4,36 @@ class Solution:
         scan through the grid when we find a 1 we do dfs lookng for connecting ones in 4 dir,
         keep a global max counter counting the area
         '''
+        
+        R, C = len(grid), len(grid[0])
         max_area = 0
-     
+        self.cur_area = 0
+        
         def dfs(r,c):
-            #counts the first 1
-            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0:
-                    return 0
             #mark as visited
             grid[r][c] = 0
-            print(r,c)
-            return 1 + dfs(r-1,c) + dfs(r+1,c) + dfs(r,c-1) + dfs(r,c+1)
+            self.cur_area += 1
+            #traverse all 4 directions
+            for dr,dc in [(-1,0), (0,-1), (1,0), (0,1)]:
+                row,col = r + dr, c + dc
+                if row < 0 or row == R or col < 0 or col == C or grid[row][col] == 0:
+                    continue
+                dfs(row, col)
         
-        max_area = 0
         
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
+        
+        for r in range(R):
+            for c in range(C):
                 if grid[r][c] == 1:
-                    max_area = max(dfs(r,c), max_area)
-                    
-                    
+                    self.cur_area = 0
+                    dfs(r,c)
+                    max_area = max(max_area, self.cur_area)
+                 
+        
         return max_area
-                
                     
-    
+        
+
                 
                 
         
