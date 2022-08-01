0227c63d9217150fc4ccedcfb6cf47216be82aeb
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-08-01 15:25:00 -0700

    Time: 56 ms (32.81%), Space: 14.3 MB (6.69%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,22 +1,20 @@
 class Solution:
     def uniquePaths(self, m, n):
         memo = {}
-        
         def dp(r,c):
-            if r == 1 and c == 1:
-                return 1
             if r == 0 or c == 0:
                 return 0
             
+            if r == 1 and c == 1:
+                return 1
+            
             if (r,c) not in memo:
-                memo[(r,c)] = dp(r-1,c) + dp(r, c-1)
+                memo[(r,c)] = dp(r-1, c) + dp(r,c-1)
             
             return memo[(r,c)]
         
+        
         return dp(m,n)
-        
-        
-        
         
         
         
