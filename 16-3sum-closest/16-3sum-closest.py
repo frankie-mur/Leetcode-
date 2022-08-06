2596546f954421229a7210c1ff5aa0fc272d0e7c
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-08-06 13:58:58 -0700

    Time: 7028 ms (42.96%), Space: 14.1 MB (43.69%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.py
@@ -0,0 +1,34 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        if not nums: return []                      
+        min_closest = math.inf
+        nums.sort()
+        res = 0
+        
+        def find_target(num, idx):
+            l = idx + 1
+            r = len(nums) - 1
+            
+            while l < r:
+                three_sum = num + nums[l] + nums[r]
+                distance = abs(target-three_sum)
+                nonlocal min_closest
+                if distance < abs(min_closest):
+                    min_closest = target - three_sum
+                    
+                if three_sum > target:
+                    r -= 1
+                else:
+                    l += 1
+                
+              
+          
+            
+                    
+        for idx,num in enumerate(nums):            
+            find_target(num, idx)
+            if min_closest == 0:
+                break
+        
+        
+        return target - min_closest
