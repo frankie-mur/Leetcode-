b344f4a32b8e75a0d0dfad820e5ef45735e6cdb5
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-08-23 15:21:06 -0700

    Time: 1252 ms (78.64%), Space: 25 MB (38.55%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxP = 0
-        start, end = 0,0
-        while end < len(prices):            
-            if prices[start] > prices[end]:
-                start = end
+        '''
+        U - Given stock prices array we want to return the best profit, we must buy on a single day and sell after. 
+        M - Two pointer, greedy
+        P - We can be gready here, we want to buy low and sell high, therfore if out left pointer is larger than our right we can move our left to our right, 
+        we calculate the largest profit as we move along
+        I - Python
+        R - Passes test casses
+        E - Time O(n)
+            Space O(1)
+        
+        '''
+        n = len(prices)
+        max_profit = 0
+        L = 0
+        
+        for R in range(1, n):
+            buy, sell = prices[L], prices[R]
+            profit = sell - buy
+            if buy > sell:
+                L = R
             else:
-                maxP = max(maxP, prices[end] - prices[start])
-                end += 1
-
-        return maxP
+                max_profit = max(max_profit, profit)
+        
+        return max_profit 
+            
         
         
         
