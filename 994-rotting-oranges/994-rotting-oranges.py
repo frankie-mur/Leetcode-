29dd9a73900bf8f4bca288925c088adfc01ea5a7
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-02 15:57:15 -0700

    Time: 68 ms (72.99%), Space: 14.3 MB (11.24%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,42 +1,41 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        #multisource bfs
-        #first find all the rotten oranges
-        R, C = len(grid), len(grid[0])
-        origin = deque()
-        fresh = 0
-        minutes = 0
-        
-        for r in range(R):
-            for c in range(C):
-                if grid[r][c] == 2:
-                    origin.append((r,c))
-                elif grid[r][c] == 1:
-                    fresh += 1
-        
-        DIRS = [(-1,0), (0,-1), (1, 0), (0, 1)]
-        
-        def is_valid(r, c):
+        def is_valid(r:int,c:int) -> bool:
             if c >= 0 and c < C and r >= 0 and r < R and grid[r][c] == 1:
                 return True
             return False
         
-        while origin and fresh > 0:
-            size = len(origin)
+        
+        R, C = len(grid), len(grid[0])
+        rotten = deque([])
+        DIRS = [(-1,0), (0,-1), (1,0), (0,1)]
+        fresh, minutes = 0, 0
+        
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == 1:
+                    fresh += 1
+                elif grid[r][c] == 2:
+                    rotten.append((r,c))
+        
+        
+        while rotten and fresh:
+            size = len(rotten)
             for _ in range(size):
-                r, c = origin.popleft()
-                
+                r, c = rotten.popleft()
+                print(r,c)
                 for dr, dc in DIRS:
                     newr, newc = r + dr, c + dc
                     if is_valid(newr, newc):
-                        grid[newr][newc] = 2
-                        origin.append((newr, newc))
                         fresh -= 1
-                
+                        grid[newr][newc] = 2
+                        rotten.append((newr, newc))
+            
             minutes += 1
-                        
-                
-                
+        
         return minutes if fresh == 0 else -1
+            
+            
+            
         
         
