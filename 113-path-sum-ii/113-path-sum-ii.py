ac362af3e040ed43cd5bed4f11da4773d5b0bc26
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-02 17:25:07 -0700

    Time: 103 ms (6.63%), Space: 19.1 MB (29.18%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -7,24 +7,25 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         
-        def dfs(node, cur_path, cur_sum):
+        res = []
+        
+        def dfs(node: Optional[TreeNode], cur_path: list, curr_sum: int) -> None:
             if not node:
                 return
             
-            cur_sum += node.val
+            curr_sum += node.val
             
-            if not node.left and not node.right and cur_sum == targetSum:
-                cur_path += [node.val]
-                res.append(cur_path)
-                #return
-                
-            dfs(node.left, cur_path + [node.val], cur_sum)
-            dfs(node.right, cur_path + [node.val], cur_sum)
+            if not node.left and not node.right and curr_sum == targetSum:
+                res.append(cur_path + [node.val])
             
+            
+            dfs(node.left, cur_path + [node.val], curr_sum)
+            dfs(node.right, cur_path + [node.val], curr_sum)
         
-        res = []
+        
         dfs(root, [], 0)
+        
         return res
-                
+            
             
             
