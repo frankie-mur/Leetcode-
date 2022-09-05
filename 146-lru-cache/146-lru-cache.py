cb2a1af3495979b4af8a6212e0d87a50f8d3779f
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-04 22:01:13 -0700

    Time: 2238 ms (8.71%), Space: 76.6 MB (24.94%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,59 +1,58 @@
-class LLNode:
+class LinkedList():
     def __init__(self, key=-1, val=-1):
         self.key = key
         self.val = val
-        self.next = None
-        self.prev = None
+        self.left = None
+        self.right = None
+
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        #key: key, val: pointer to the node in the LL
         self.cache = {}
-        self.MRU = LLNode()
-        self.LRU = LLNode()
-        #now connect them
-        self.MRU.prev = self.LRU
-        self.LRU.next = self.MRU
+        self.LRU = LinkedList()
+        self.MRU = LinkedList()
+        self.LRU.right = self.MRU
+        self.MRU.left = self.LRU
         
     def get(self, key: int) -> int:
         if key in self.cache:
-            #update the node bc we just used it 
             node = self.cache[key]
             self.delete(node)
             self.insert(node)
             return node.val
         else:
             return -1
+        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            #delete and update value
-            self.delete(self.cache[key])    
-        self.cache[key] = LLNode(key, value)
-        self.insert(self.cache[key])
+            self.delete(self.cache[key])
         
+        self.cache[key] = LinkedList(key, value)
+        self.insert(self.cache[key])
+    
         if len(self.cache) > self.capacity:
-            lru = self.LRU.next
-            self.delete(lru)
-            del self.cache[lru.key]
+            LRU = self.LRU.right
+            self.delete(LRU)
+            del self.cache[LRU.key]
     
-    def insert(self, node: LLNode):
-        #insert inbetween MRU and LRU
-        prev, nxt = self.MRU.prev, self.MRU
-        prev.next = node
-        nxt.prev = node
-        node.next = nxt
-        node.prev = prev
+    def insert(self, node) -> None:
+        #insert bewtween MRU and right
+        prev, nxt = self.MRU.left, self.MRU
+        prev.right = node
+        nxt.left = node
+        node.left = prev
+        node.right = nxt
     
-    def delete(self, node: LLNode):
-        prev = node.prev
-        nxt = node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-
+    def delete(self, node) -> None:
+        prev = node.left
+        nxt = node.right
+        prev.right = nxt
+        nxt.left = prev
+        
+    
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
