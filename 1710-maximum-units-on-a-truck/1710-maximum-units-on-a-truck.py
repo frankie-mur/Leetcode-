66d9ff5b2fd47927e15eb76bc68106dad0346420
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-05 14:56:20 -0700

    Time: 198 ms (76.59%), Space: 14.4 MB (31.81%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,20 +1,26 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        sorted_box = sorted(boxTypes, key=lambda x:x[1], reverse=True)
-        res,i = 0,0
-        while truckSize > 0:
-            #print(i)
-            boxes,units = sorted_box[i]
-            #print(truckSize)
-            #print(sorted_box)
-            if truckSize >= boxes:
-                res += (boxes * units)
-                truckSize -= boxes
-                i += 1
-            else:
-                res += (truckSize * units)
-                truckSize = 0
-            if i == len(sorted_box):
+#         #sort by unitsPbox / numBox'es
+#         def calc_units_per_box(num_boxes, boxes_per_unit):
+#             return boxes_per_unit / num_boxes
+        
+#         sorted_ = []
+#         for num, units in boxTypes:
+#             sorted_.append((calc_units_per_box(num, units), (num, units)))
+        
+        boxTypes.sort(key=lambda x:x[1], reverse=True)
+        
+        max_units = 0
+        for boxes, units in boxTypes:
+            # boxes, units = truck
+            sub = min(boxes, truckSize)
+            max_units += (units * sub)
+            truckSize -= sub
+            if truckSize == 0:
                 break
+        
+        return max_units
             
-        return res
+            
+            
+            
