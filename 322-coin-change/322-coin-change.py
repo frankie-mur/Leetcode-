5dc502d6a961508ff638e62382783ed8ffd744d7
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-05 23:13:02 -0700

    Time: 1233 ms (94.38%), Space: 15.1 MB (28.83%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,30 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        
+        q = deque()
+        q.append(amount) # amt, depth
+        visited = set()
+        depth = 0
+        while q:
+            for i in range(len(q)):
+                amt = q.popleft()
+                
+
+                if amt < 0: # skip prune branches that yeild -ve nodes
+                    continue
+
+                elif amt == 0:
+                    return depth
+
+                if amt not in visited: # skip nodes seen before - see explanantion above
+                    visited.add(amt)
+
+                    # move down a level
+                    for c in coins: 
+                        q.append(amt-c)
+
+            depth += 1
+
+        return -1
