ca9289351d4de06cfd647ef4bbff0e3e82ad460e
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-06 16:01:45 -0700

    Time: 969 ms (8.73%), Space: 14.7 MB (57.23%) - LeetHub

---
+++ 490-the-maze/490-the-maze.py
@@ -0,0 +1,38 @@
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        #bfs?
+        R, C = len(maze), len(maze[0])
+        visited = set()
+        q = deque([start])
+        DIRS = [(-1,0), (0,-1), (1,0), (0,1)]
+        
+        def roll(r, c, dr, dc):
+            while 0 <= r+dr < R and 0 <= c+dc < C and maze[r+dr][c+dc] == 0:
+                r += dr
+                c += dc
+            
+            return r, c
+        
+        while q:
+            r, c = q.popleft()
+            visited.add((r,c))
+            #print(r,c)
+            if [r,c] == destination:
+                return True
+            
+            for dr, dc in DIRS:
+                newr, newc = roll(r, c, dr, dc)
+                #print(newr, newc)
+                if (newr, newc) not in visited:
+                    # visited.add((newr, newc))
+                    q.append((newr,newc))
+        
+        
+        return False
+                
+                
+                
+            
+        
+        
+        
