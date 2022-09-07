1ed75ea5aa7125d98d087fe3887d71e4b27bd577
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-07 16:57:18 -0700

    Time: 32 ms (97.16%), Space: 14 MB (50.02%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,16 +1,24 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        '''
-        U - Find the longest common prefix among an array of strings
-        M - Pointers all across?, or use cool zip pattern
-        P - 
+        res = ""
         
-        '''
+        min_str = min([len(s) for s in strs])
+        start = strs[0]
+        
+        for idx in range(min_str):
+            for s in strs:
+                if start[idx] != s[idx]:
+                    return res
+            
+            res += start[idx]
+        
+        return res
+                
+
+
+            
+
         
 
-        pre = strs[0]
-        for i in strs:
-            while not i.startswith(pre):
-                pre = pre[:-1]
             
-        return pre     
+        
