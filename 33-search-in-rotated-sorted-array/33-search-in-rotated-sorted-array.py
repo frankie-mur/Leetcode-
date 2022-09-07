2c867240bcbfafd604c6f1a3a94068ebabd611e6
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-07 13:50:07 -0700

    Time: 76 ms (29.53%), Space: 14.2 MB (90.91%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,29 +1,47 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        '''
-        Binary search
-        '''
-        l, r = 0, len(nums) - 1
-        
-        while l <= r:
-            mid = l + (r - l) // 2    
-            cur = nums[mid]
-            if target == cur:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            # if found target value, return the index
+            if nums[mid] == target:
                 return mid
-            #logic for left side
-            if cur >= nums[l]:
-                if target > cur or target < nums[l]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            else:
-                if target < cur or target > nums[r]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-        
-        return -1
             
+            # determine it's left rotated or right rotated
+            """
+            No rotated:
+            1 2 3 4 5 6 7
+                 mid
+                 
+            left rotated: pivot at the left side of the origin sorted array, A[mid] >= A[left]
+            3 4 5 6 7 1 2
+                 mid
+            search in A[left] ~ A [mid] if A[left] <= target < A[mid] else, search right side
+            
+            right rotated: pivot at the right side of the origin sorted array, A[mid] < A[left]
+            6 7 1 2 3 4 5
+                 mid          
+            search in A[mid] ~ A[right] if A[mid] < target <= A[right] else, search left side
+            """
+            if nums[mid] >= nums[left]: # left rotated
+                # in ascending order side
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else: # right rotated
+                # in ascending order side
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        # cannot find the target value
+        return -1
             
     
                 
