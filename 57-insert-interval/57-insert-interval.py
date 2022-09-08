b8d201bbb83c4a9e38b13ecde94e4fdf32d3bba3
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-07 21:52:28 -0700

    Time: 105 ms (72.18%), Space: 17.4 MB (52.72%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,23 +1,20 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        #add interval than merge
-        intervals.append(newInterval)
-        intervals.sort()
-        
-        start, end = intervals[0][0], intervals[0][1]
         res = []
         
-        for i in range(1, len(intervals)):
-            nxt_start, nxt_end = intervals[i][0], intervals[i][1]
-            #print(end, nxt_start)
-            if end >= nxt_start:
-                end = max(end, nxt_end)
+        #only the new interval is merged
+        for i in range(len(intervals)):
+            start, end = intervals[i]
+            if start > newInterval[1]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif end < newInterval[0]:
+                res.append(intervals[i])
             else:
-                res.append([start,end])
-                start = nxt_start
-                end = nxt_end
-            
-        res.append([start,end])
-            
-        return res
+                newInterval = [min(start, newInterval[0]), max(end, newInterval[1])]
+    
         
+        res.append(newInterval)
+        
+        return res
+                
