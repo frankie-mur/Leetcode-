c4d4e10050423581c070c5130f799b1f619e2d48
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-10 14:38:30 -0700

    Time: 50 ms (97.00%), Space: 13.9 MB (91.46%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,41 +1,43 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        def is_valid(r:int,c:int) -> bool:
-            if c >= 0 and c < C and r >= 0 and r < R and grid[r][c] == 1:
-                return True
-            return False
-        
-        
-        R, C = len(grid), len(grid[0])
         rotten = deque([])
+        R, C = len(grid), len(grid[0])
+        minutes = 0
         DIRS = [(-1,0), (0,-1), (1,0), (0,1)]
-        fresh, minutes = 0, 0
+        fresh = 0
         
         for r in range(R):
             for c in range(C):
-                if grid[r][c] == 1:
-                    fresh += 1
-                elif grid[r][c] == 2:
+                if grid[r][c] == 2:
                     rotten.append((r,c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+        
+        def is_valid(r,c):
+            if 0 <= c < C and 0 <= r < R and grid[r][c] == 1:
+                return True
+            return False
         
         
         while rotten and fresh:
             size = len(rotten)
             for _ in range(size):
-                r, c = rotten.popleft()
-                print(r,c)
-                for dr, dc in DIRS:
-                    newr, newc = r + dr, c + dc
-                    if is_valid(newr, newc):
+                r,c = rotten.popleft()
+                #print(r,c, minutes)
+                for dr,dc in DIRS:
+                    if is_valid(r + dr, c + dc):
+                        grid[r + dr][c + dc] = 2
                         fresh -= 1
-                        grid[newr][newc] = 2
-                        rotten.append((newr, newc))
-            
+                        rotten.append((r + dr, c + dc))
+                
             minutes += 1
+    
+        if fresh == 0:
+            return minutes
+        return -1
         
-        return minutes if fresh == 0 else -1
-            
-            
-            
-        
-        
+
+
+
+
+
