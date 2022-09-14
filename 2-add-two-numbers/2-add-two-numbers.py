386f1f7905341569596e8d225d33d0947b4546db
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-14 14:07:56 -0700

    Time: 193 ms (5.01%), Space: 13.9 MB (43.22%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,28 +5,25 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        #logicaly
-        total = 0
+        dummy = ListNode
+        cur = dummy
         carry = 0
-        dum = res = ListNode()
-        
-        #while theres still values in linked list or if theres a carry remaining
         while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-            total = v1 + v2 + carry
-            #you dont need to check if carry > 9 bc if less than 10 will eval to 0
-            # // 10 will get the first digit in any int
+            l1val = l1.val if l1 else 0
+            l2val = l2.val if l2 else 0
+        
+            total = l1val + l2val + carry
             carry = total // 10
-            #total only needs the ones place
+            #print(total , carry)
             total = total % 10
-            #add the value to the resulting LinkedList
-            res.next = ListNode(total)
-            res = res.next
+            
+            cur.next = ListNode(total)
+            cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        
-        
-        return dum.next
+            
+        return dummy.next
+            
+            
                 
         
