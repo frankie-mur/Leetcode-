f37bab9002101d268d7857b71622029faca28484
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-15 15:20:03 -0700

    Time: 2559 ms (28.56%), Space: 18.1 MB (39.05%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,32 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if not nums: return []                      
-        res = []
         nums.sort()
+        res = []
+        n = len(nums)
         
-        def find_target(num, idx):
+        def find_target(idx, num):
             l = idx + 1
-            r = len(nums) - 1
+            r = n - 1
             
             while l < r:
-                three_sum = num + nums[l] + nums[r]
-                if three_sum > 0:
-                    r -= 1
-                elif three_sum < 0:
+                three_sum = nums[idx] + nums[l] + nums[r]
+                if three_sum == 0:
+                    #result
+                    res.append([nums[idx], nums[l], nums[r]])
                     l += 1
-                else:
-                    res.append([num,nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l-1] and l < r:
+                    while nums[l] == nums[l - 1] and l < r:
                         l += 1
-                        
+                elif three_sum > 0:
+                    r -= 1
+                else:  
+                    l += 1
         
-        for idx,num in enumerate(nums):
-            #duplicate
-            if idx > 0 and nums[idx] == nums[idx-1]:
+        
+        
+        for idx, num in enumerate(nums):
+            if idx > 0 and nums[idx] == nums[idx - 1]:
                 continue
-            
-            find_target(num, idx)
-        
+            find_target(idx, num)
         
         return res
