9592760a3b889272a0f171ea74e301d01ea8dc8d
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-15 14:28:13 -0700

    Time: 94 ms (99.09%), Space: 15.4 MB (89.71%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,28 +1,29 @@
 class Solution:
-    def canFinish(self, numCourses: int, preqs: List[List[int]]) -> bool:
-        #top sort khans
+    def canFinish(self, numCourses: int, pre: List[List[int]]) -> bool:
+        #top sort
         graph = defaultdict(list)
         indegree = {i:0 for i in range(numCourses)}
-        for u, v in preqs:
-            graph[v].append(u)
-            indegree[u] += 1
+        for pre, class_ in pre:
+            graph[pre].append(class_)
+            indegree[class_] += 1
         
-        source = deque([])
+        source = deque()
         
-        for k, v in indegree.items():
-            if v == 0:
-                source.append(k)
+        for key,val in indegree.items():
+            if val == 0:
+                source.append(key)
         
-        counter = 0
+        classes = 0
         while source:
-            node = source.popleft()
-            counter += 1
-            for nei in graph[node]:
+            course = source.pop()
+            classes += 1
+            for nei in graph[course]:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     source.append(nei)
         
-        return counter == numCourses
+        return classes == numCourses
+        
         
         
             
