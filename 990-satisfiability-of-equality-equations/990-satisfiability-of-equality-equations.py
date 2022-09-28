0d9bf69093a3f6dd2145f43f3d29573b03e6582a
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-09-27 17:19:48 -0700

    Time: 98 ms (36.39%), Space: 14.2 MB (36.29%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -1,45 +1,38 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        #union find!
+        #UF
         parents = [i for i in range(26)]
+        
         def find(x):
-            if parents[x] != x:
+            if x != parents[x]:
                 parents[x] = find(parents[x])
-            
             return parents[x]
         
         def union(x, y):
-            a = find(x)
-            b = find(y)
+            x = find(x)
+            y = find(y)
             
-            parents[a] = b
+            parents[y] = x
+        
         
         for eq in equations:
             v1, v2, op = eq[0], eq[3], eq[1]
             if op == "!":
                 continue
+            v1 = ord(v1) - ord('a')
+            v2 = ord(v2) - ord('a')
             
-            a = ord(v1) - ord('a')
-            b = ord(v2) - ord('a')
-            
-            union(a,b)
+            union(v1, v2)
         
         for eq in equations:
             v1, v2, op = eq[0], eq[3], eq[1]
             if op == "=":
                 continue
-            
-            a = ord(v1) - ord('a')
-            b = ord(v2) - ord('a')
-            
-            if find(a) == find(b):
+                
+            v1 = ord(v1) - ord('a')
+            v2 = ord(v2) - ord('a')
+        
+            if find(v1) == find(v2):
                 return False
-        
+    
         return True
-            
-            
-            
-            
-            
-            
-        
