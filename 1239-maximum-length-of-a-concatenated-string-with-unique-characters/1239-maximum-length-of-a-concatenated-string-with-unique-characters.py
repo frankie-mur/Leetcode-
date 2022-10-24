9ce6680288558d72c780865debac2ced630772b5
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-10-23 18:07:24 -0700

    Time: 167 ms (73.15%), Space: 13.8 MB (90.64%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,22 @@
+class Solution:
+	def maxLength(self, arr):
+		self.maximum = 0
+
+		def backtrack(start,array,s):
+			if len(s)==len(set(s)):
+				self.maximum = max(self.maximum,len(s))
+			else:
+				return
+			for i in range(start,len(array)):
+				backtrack(i+1,array,s + array[i])
+
+		backtrack(0,arr,"")
+		return self.maximum
+                    
+                        
+                    
+                    
+                
+                    
+                
+            
