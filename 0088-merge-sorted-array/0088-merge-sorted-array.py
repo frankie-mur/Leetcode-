bd768b71c638aa758c9f0504b6905ef29ce16e87
Frankie Murillo <34355192+frankidatank@users.noreply.github.com>
2022-10-28 10:19:15 -0700

    Time: 50 ms (79.56%), Space: 13.8 MB (85.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,17 +7,16 @@ class Solution:
         #two pointer starting from back
         
         p1, p2 = m - 1, n - 1
-
-        for cur in range((m + n) - 1, -1, -1):
-            if p2 < 0:
-                break
+        cur = (m + n) - 1
+        while p2 >= 0:
             if p1 >= 0 and nums1[p1] > nums2[p2]:
                 nums1[cur] = nums1[p1]
                 p1 -= 1
             else:
                 nums1[cur] = nums2[p2]
                 p2 -= 1
-                
+              
+            cur -= 1
             
         
         
